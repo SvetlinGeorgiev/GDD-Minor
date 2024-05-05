6642c879185f2edb55d20ca6e7e45b30342931fc
Svetlin Georfiev <svetlin2002@gmail.com>
2024-05-06 01:56:18 +0300

    Some changes and old code removed

--- GDD Minor/Assets/Lilac/LilacScripts/MainMenu.cs
+++ GDD Minor/Assets/Lilac/LilacScripts/MainMenu.cs
@@ -6,20 +6,6 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
    
-    public string gameSceneName = "Svetlin-testing-safe-game";
-
-    
-    public DataPersistenceManager dataPersistenceManager;
-
-
-    private void Start() 
-    {
-        //if (!DataPersistenceManager.instance.HasGameData()) 
-        //{
-        //    continueGameButton.interactable = false;
-        //}
-    }
-
     public void LoadGameScene()
     {
         
@@ -29,13 +15,7 @@ public class MainMenu : MonoBehaviour
 
     public void NewGame()
     {
-        
-        //DisableMenuButtons();
-        // create a new game - which will initialize our game data
         DataPersistenceManager.instance.NewGame();
-        // load the gameplay scene - which will in turn save the game because of
-        // OnSceneUnloaded() in the DataPersistenceManager
         SceneManager.LoadSceneAsync("Svetlin-testing-safe-game");
-        
     }
 }
